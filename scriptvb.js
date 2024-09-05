4d709c640c37c80665effac7ea6bd3ddbfa737ef
Sam Leff <133615358+shalomyehudaf@users.noreply.github.com>
2024-09-04 22:05:21 -0400

    Add files via upload

---
+++ scriptvb.js
@@ -0,0 +1,14 @@
+function fetchanddisplay(url){
+    fetch(url)
+        .then(response => response.text())
+        .then(data => {
+            const filecontent = document.getElementById('file-content');
+            filecontent.textContent = data;
+        })
+        .catch(error => {
+            console.error('Error fetching file:', error);
+            const filecontent = document.getElementById('file-content');
+            filecontent.textContent = 'Error loading file content.';
+        })
+}
+fetchanddisplay('examplevb.html');
